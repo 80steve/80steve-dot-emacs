1d26a4c7adb8fb59a423cd1db9c6c8f26f0c6c57
Steve Chan <gh@80steve.com>
2012-11-16 00:22:00 +0800

    Updated yasnippet-setup to remove obsolete functions

--- p/yasnippet-setup.el
+++ p/yasnippet-setup.el
@@ -1,5 +1,5 @@
 (require 'yasnippet)
-(setq yas/snippet-dirs "~/.emacs.d/vendor/yasnippet/snippets")
-(yas/global-mode 1)
+(setq yas-snippet-dirs "~/.emacs.d/vendor/yasnippet/snippets")
+(yas-global-mode 1)
 
 (provide 'yasnippet-setup)
