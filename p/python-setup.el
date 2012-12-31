0f188820be20392747a2be2544efe09f9f6a82ab
Steve Chan <gh@80steve.com>
2013-01-01 00:52:33 +0800

    Improved python setup abit with indentation highlight & rope project default

--- p/python-setup.el
+++ p/python-setup.el
@@ -2,13 +2,29 @@
 (require 'python-mode)
 
 (require 'pymacs)
-(autoload 'pymacs-apply "pymacs")
-(autoload 'pymacs-call "pymacs")
-(autoload 'pymacs-eval "pymacs" nil t)
-(autoload 'pymacs-exec "pymacs" nil t)
-(autoload 'pymacs-load "pymacs" nil t)
+;(autoload 'pymacs-apply "pymacs")
+;(autoload 'pymacs-call "pymacs")
+;(autoload 'pymacs-eval "pymacs" nil t)
+;(autoload 'pymacs-exec "pymacs" nil t)
+;(autoload 'pymacs-load "pymacs" nil t)
 (pymacs-load "ropemacs" "rope-")
+(setq ropemacs-codeassist-maxfixes 3)
+(setq ropemacs-guess-project t)
 (setq ropemacs-enable-autoimport t)
-(setq rope-generate-autoimport-cache t)
+
+;; Auto Search for rope project directory for once
+;; current or upper directory
+(add-hook 'python-mode-hook
+          (lambda ()
+            (cond ((file-exists-p ".ropeproject")
+                   (rope-open-project default-directory))
+                  ((file-exists-p "../.ropeproject")
+                   (rope-open-project (concat default-directory "..")))
+                  )))
+
+(require 'highlight-indentation)
+(add-hook 'python-mode-hook 'highlight-indentation)
+(set-face-background 'highlight-indentation-face "#454545")
+(set-face-background 'highlight-indentation-current-column-face "#8A8A8A")
 
 (provide 'python-setup)
