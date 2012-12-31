bab287f1c1ef7cb442a8f6bb6883779f95db9cfd
Steve Chan <gh@80steve.com>
2013-01-01 00:53:16 +0800

    Autocomplete dropdown in eshell & minor tweaks

--- p/eshell-setup.el
+++ p/eshell-setup.el
@@ -1,3 +1,18 @@
+(setq
+ eshell-save-history-on-exit t
+ eshell-history-size 128
+ eshell-hist-ignoredups t
+ eshell-cmpl-ignore-case t
+ eshell-cp-interactive-query t
+ eshell-mv-interactive-query t
+ eshell-rm-interactive-query t)
+
+(require 'auto-complete)
+(defvar ac-source-eshell-pcomplete
+  '((candidates . (pcomplete-completions))))
+(add-to-list 'ac-modes 'eshell-mode)
+(setq ac-sources '(ac-source-eshell-pcomplete))
+
 (defun eshell/clear ()
   "Clears the shell buffer ala Unix's clear or DOS' cls"
   (interactive)
