5f4909f0799e54987eb52d261bcfad4abc521619
Steve Chan <gh@80steve.com>
2013-01-13 21:51:57 +0800

    Adapted to use Emacs24 color theme

--- p/appearance.el
+++ p/appearance.el
@@ -1,9 +1,9 @@
-(require 'color-theme)
-;(color-theme-initialize) ;; not necessary if default theme not required
-(load-file "~/.emacs.d/vendor/color-theme-tomorrow.el")
-(color-theme-tomorrow-night)
+(require 'monokai-theme)
 (set-face-background 'hl-line "grey20")
 (set-face-background 'region "#2F133B") ;; Search Highlight Color
 (set-frame-font "Droid_Sans_Mono-12")
+
+(require 'powerline)
+(powerline-default)
 
 (provide 'appearance)
