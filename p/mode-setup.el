f9edb306c50c03208d3479435e7d4389faffa6f0
Steve Chan <gh@80steve.com>
2013-06-27 23:40:56 +0800

    Default webmode for tempalte dev (html)

--- p/mode-setup.el
+++ p/mode-setup.el
@@ -1,6 +1,8 @@
+(autoload 'c-mode "c-setup" "C Mode." t)
+(autoload 'c++-mode "c-setup" "C Mode." t)
 (autoload 'php-mode "php-setup" "PHP mode." t)
 (autoload 'python-mode "python-setup" "Python Mode." t)
-;(autoload 'js2-mode "js2" "Javascript Mode." t)
+(autoload 'js2-mode "js2" "Javascript Mode." t)
 (autoload 'multi-web-mode "multi-web-setup" "Multi Web Mode." t)
 
 (setq auto-mode-alist (cons '("\\.php$" . php-mode) auto-mode-alist))
@@ -8,6 +10,10 @@
 (setq auto-mode-alist (cons '("\\.inc$" . php-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.install$" . php-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.engine$" . php-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.html$" . multi-web-mode) auto-mode-alist))
+
+(require 'web-mode)
+(setq web-mode-disable-auto-pairing t)
+
+(setq auto-mode-alist (cons '("\\.html$" . web-mode) auto-mode-alist))
 
 (provide 'mode-setup)
