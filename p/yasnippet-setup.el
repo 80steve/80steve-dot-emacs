bed3bfd417362b2507dfe291a624922343e95c42
Steve Chan <gh@80steve.com>
2013-09-01 12:26:50 +0800

    Adjust yasnippet prompt

--- p/yasnippet-setup.el
+++ p/yasnippet-setup.el
@@ -1,8 +1,9 @@
 (require 'yasnippet)
 (setq yas-snippet-dirs "~/.emacs.d/vendor/yasnippet/snippets")
 (yas-global-mode 1)
+(require 'dropdown-list)
 (setq yas-prompt-functions '(yas-dropdown-prompt
-                             yas-ido-prompt
-                             yas-completing-prompt))
+                             yas-completing-prompt
+                             yas-ido-prompt))
 
 (provide 'yasnippet-setup)
