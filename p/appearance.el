be152593f75ce6090223747b5785392bef4cf0f5
Steve Chan <gh@80steve.com>
2014-04-06 18:14:58 +0800

    Default to solarized theme

--- p/appearance.el
+++ p/appearance.el
@@ -1,10 +1,13 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/vendor/solarized")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/vendor/themes")
-(load-theme 'tomorrow-night t)
+(load-theme 'solarized-dark t)
 
-(set-frame-font "Droid_Sans_Mono-12")
+(set-face-attribute 'default nil :family "Droid_Sans_Mono")
 
 (require 'powerline)
-(powerline-default-theme)
+(custom-set-faces
+ '(header-line ((t (:inherit hl-line))))
+ '(magit-item-highlight ((t (:inherit hl-line))))
+ '(mode-line ((t (:foreground "gray10" :background "OliveDrab3" :box nil)))))
 
 (provide 'appearance)
