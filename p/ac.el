0ac93e2d57507b958f18d001296fb0de0e8cd5e8
Steve Chan <gh@80steve.com>
2015-10-06 11:14:03 +0800

    UPDATE to finetune settings

--- p/ac.el
+++ p/ac.el
@@ -7,14 +7,14 @@
 (ac-config-default)
 (global-auto-complete-mode t)
 (setq ac-dwim t)
-(setq ac-delay 0.2)
-(setq ac-auto-show-menu t)
-(setq ac-quick-help-delay 0.2)
+(setq ac-delay 0.3)
+(setq ac-auto-show-menu 0.6)
+(setq ac-quick-help-delay 1)
 (setq ac-source-yasnippet nil)
+(setq ac-auto-start 2)
 (define-key ac-complete-mode-map "\C-n" 'ac-next)
 (define-key ac-complete-mode-map "\C-p" 'ac-previous)
 (define-key ac-completing-map "\e" 'ac-stop)
-(setq ac-auto-start t)
 
 (provide 'ac)
 ;;; ac.el ends here
