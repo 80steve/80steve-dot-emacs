e27db21c5206ebac226f155e8ac216582ff21cf9
Steve Chan <gh@80steve.com>
2016-04-05 00:47:54 +0800

    UPDATE to use spacemacs theme

--- p/appearance.el
+++ p/appearance.el
@@ -1,22 +1,13 @@
 ;;; appearance.el --- General Appearance of Emacs
 ;;; Code:
 ;;; Commentary:
-(load-theme 'monokai t)
+(load-theme 'spacemacs-dark t)
 
-(set-face-attribute 'default nil :family "Droid_Sans_Mono")
-(set-fontset-font "fontset-default"
-                  'unicode
-                  '("Heiti TC"))
+(set-face-attribute 'default nil :family "Menlo")
 
 (require 'powerline)
 (powerline-default-theme)
 (setq powerline-default-separator 'contour)
-(custom-set-faces
- '(powerline-active1 ((t (:background "gray15" :foreground "white"))))
- '(powerline-active2 ((t (:background "DarkSlateGray" :foreground "white"))))
- '(powerline-inactive1 ((t (:background "gray15" :foreground "gray50"))))
- '(mode-line ((t (:background "DarkSlateGray" :foreground "gray80"))))
- '(mode-line-buffer-id ((t (:foreground "gray80")))))
 
 (require 'git-gutter)
 (global-git-gutter-mode)
