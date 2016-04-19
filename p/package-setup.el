0667eac6b8189da351b47c382d7e764216756218
Steve Chan <gh@80steve.com>
2016-04-19 22:31:06 +0800

    UPDATE to add the missing ag package

--- p/package-setup.el
+++ p/package-setup.el
@@ -13,7 +13,7 @@
 ;; Adopted from http://batsov.com/articles/2012/02/19/package-management-in-emacs-the-good-the-bad-and-the-ugly/
 
 (defvar editor-packages
-  '(ace-jump-mode anzu browse-kill-ring coffee-mode company company-go company-jedi company-quickhelp company-tern  dedicated diminish dropdown-list
+  '(ace-jump-mode ag anzu browse-kill-ring coffee-mode company company-go company-jedi company-quickhelp company-tern dedicated diminish dropdown-list
                   expand-region flycheck flycheck-color-mode-line git-gutter git-timemachine go-mode helm helm-projectile
                   highlight-indentation idle-highlight-mode js2-mode less-css-mode magit markdown-mode monokai-theme multiple-cursors
                   php-mode powerline prodigy projectile python-django rainbow-delimiters react-snippets rainbow-mode smartparens virtualenvwrapper
