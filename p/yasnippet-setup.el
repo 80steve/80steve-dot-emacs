22d75885340d75b007c9d985791b105169ab5bba
Steve <steve@betamatters.com>
2019-02-06 19:33:53 +0800

    REMOVE dropdown list dependencies

--- p/yasnippet-setup.el
+++ p/yasnippet-setup.el
@@ -4,9 +4,7 @@
 (require 'yasnippet)
 
 (yas-global-mode 1)
-(require 'dropdown-list)
-(setq yas-prompt-functions '(yas-dropdown-prompt
-                             yas-completing-prompt
+(setq yas-prompt-functions '(yas-completing-prompt
                              yas-ido-prompt))
 
 (provide 'yasnippet-setup)
