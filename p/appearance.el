a40e37086e92280150f72516b64d5fd6e354a59d
Steve <steve@betamatters.com>
2019-02-06 19:34:10 +0800

    UPDATE default font size

--- p/appearance.el
+++ p/appearance.el
@@ -3,7 +3,7 @@
 ;;; Commentary:
 (load-theme 'spacemacs-dark t)
 
-(set-face-attribute 'default nil :family "Droid Sans Mono" :height 140)
+(set-face-attribute 'default nil :family "Droid Sans Mono" :height 110)
 
 (require 'powerline)
 (powerline-default-theme)
