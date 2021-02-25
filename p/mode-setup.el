6eff9b4187ffc84e6be5a16f39358d0c76c0f403
Steve <steve@betamatters.com>
2021-02-25 12:00:23 +0800

    UPDATE to extract lsp into its own setup

--- p/mode-setup.el
+++ p/mode-setup.el
@@ -89,51 +89,6 @@
   )
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
-(use-package lsp-mode
-  :defer t
-  :hook
-  (go-mode . lsp-deferred)
-  (js-mode . lsp-deferred)
-  (js2-mode . lsp-deferred)
-  (typescript-mode . lsp-deferred)
-  (web-mode . lsp-deferred)
-  :commands (lsp lsp-deferred)
-  :init
-  (setq lsp-keymap-prefix "C-c C-l")
-  (setq lsp-completion-enable t)
-  (setq lsp-enable-snippet t)
-  (setq lsp-keep-workspace-alive t)
-  (setq lsp-enable-xref t)
-  (setq lsp-enable-imenu t)
-  :config
-  (setq lsp-prefer-flymake nil)
-  (setq company-backends
-        '((company-files
-           company-keywords
-           company-capf
-           )
-          (company-abbrev company-dabbrev)))
-  )
-
-(use-package lsp-ui
-  :defer t
-  :requires lsp-mode flycheck
-  :init
-  (add-hook 'lsp-mode-hook 'lsp-ui-mode)
-  :config
-  (setq lsp-ui-doc-enable t)
-  (setq lsp-ui-doc-position 'top)
-  (setq lsp-ui-doc-include-signature t)
-  (setq lsp-ui-peek-enable t)
-  (setq lsp-ui-peek-always-show t)
-  (setq lsp-ui-sideline-enable nil)
-  (setq lsp-ui-flycheck-live-reporting t)
-  :bind (:map lsp-ui-mode-map
-              ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
-              ([remap xref-find-references] . lsp-ui-peek-find-references)
-              ("C-c u" . lsp-ui-imenu))
-  )
-
 
 ;; Modeline Setup
 (use-package diminish
